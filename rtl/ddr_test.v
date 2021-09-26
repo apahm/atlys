c9679374a5fbf741c77ff465aeb7b42bc56364c5
Alex Pahmutov <alexpahmutow@gmail.com>
2021-09-26 17:40:08 +0300

    debug ddr

--- rtl/ddr_test.v
+++ rtl/ddr_test.v
@@ -76,7 +76,7 @@ module ddr_test
 localparam [5:0] MAX_BURST_LENGHT = 6'd63;
 
 localparam [3:0] COMMAND_FIFO_DEPTH = 3'd4;
-localparam [3:0] WRITE_READ_FIFO_DEPTH = 7'64;
+localparam [3:0] WRITE_READ_FIFO_DEPTH = 7'd64;
 
 localparam [3:0]
     STATE_WAIT_CALIB = 4'd0,
@@ -119,16 +119,26 @@ reg         p2_rd_en_reg;
 
 // Test regs
 reg [29:0] addr_ptr_reg;
-reg [5:0]  word_count_reg;
+reg [6:0]  word_count_reg;
 reg [31:0] error_count_reg;
 reg [8:0] address_iter_reg;
 reg [7:0] led_reg;
-
 // Test memory reg
 reg [31:0] memory_reg [0:63];
 reg [31:0] memory_read_reg [0:63];
 
-assign led = led_reg;
+assign led = c3_p0_wr_count;
+
+assign c3_p0_rd_en = p0_rd_en_reg;
+
+assign c3_p0_wr_en = p0_wr_en_reg;
+assign c3_p0_wr_mask = p0_wr_mask_reg;
+assign c3_p0_wr_data = p0_wr_data_reg;
+
+assign c3_p0_cmd_en = (p0_cmd_en_reg);
+assign c3_p0_cmd_instr = p0_cmd_instr_reg;
+assign c3_p0_cmd_bl = p0_cmd_bl_reg;
+assign c3_p0_cmd_byte_addr = p0_cmd_byte_addr_reg;
 
 always @(posedge clk) begin
 	if (rst) begin
@@ -166,13 +176,13 @@ always @(posedge clk) begin
                     p0_cmd_byte_addr_reg <= addr_ptr_reg;
 
                     addr_ptr_reg <= addr_ptr_reg + 30'hFC;
- 
-                    state_reg <= STATE_WRITE;
+                    state_reg <= STATE_WAIT_FIFO_EMPTY;
                 end else begin
                     state_reg <= STATE_WRITE_COMMAND;
                 end
 			end
 			STATE_WAIT_FIFO_EMPTY: begin
+                p0_cmd_en_reg <= 1'b0;
                 if(c3_p0_wr_empty) begin
                     state_reg <= STATE_WRITE;
                 end else begin
@@ -180,20 +190,8 @@ always @(posedge clk) begin
                 end 
 			end
             STATE_WRITE: begin
-                if(word_count_reg == 6'd63)
-                    state_reg <= STATE_WRITE_COMMAND;
-                    word_count_reg <= 6'b0;
-                end else begin
-                    p0_wr_en_reg <= 1'b1;
-                    p0_wr_mask_reg <= 4'b0;
-                    p0_wr_data_reg <= memory_reg[word_count_reg];
-                    word_count_reg <= word_count_reg + 1'b1;
-                    state_reg <= STATE_WRITE;
-                end
-            end
-            STATE_WRITE: begin
-                if(word_count_reg == 6'd63)
-                    state_reg <= STATE_WRITE_COMMAND;
+                if(word_count_reg == 'd64) begin
+                    state_reg <= STATE_TEST_SUCCESSFUL;
                     p0_wr_en_reg <= 1'b0; 
                     word_count_reg <= 6'b0;
                 end else begin
@@ -226,7 +224,7 @@ always @(posedge clk) begin
                 end 
             end
             STATE_READ: begin
-                if(word_count_reg == 6'd63)
+                if(word_count_reg == 6'd63) begin
                     state_reg <= STATE_COMPARE;
                     word_count_reg <= 6'b0;
                     p0_rd_en_reg <= 1'b0;
@@ -238,7 +236,7 @@ always @(posedge clk) begin
                 end
             end
             STATE_COMPARE: begin
-                if(word_count_reg == 6'd63)
+                if(word_count_reg == 6'd63) begin
                     state_reg <= STATE_READ_COMMAND;
                     word_count_reg <= 6'b0;
                 end else begin
@@ -250,11 +248,79 @@ always @(posedge clk) begin
                 end
             end
             STATE_TEST_SUCCESSFUL: begin
-                led_reg <= error_count_reg;
+                state_reg <= STATE_TEST_SUCCESSFUL;
             end
 		endcase
 	end
 end
 
+always @(posedge clk) begin
+    if(rst) begin
+        memory_reg[0]  <= 32'h12345678;
+        memory_reg[1]  <= 32'h87654321;
+        memory_reg[2]  <= 32'h12121212;
+        memory_reg[3]  <= 32'h8A4FBCD1;
+        memory_reg[4]  <= 32'h92374DAB;
+        memory_reg[5]  <= 32'h15964278;
+        memory_reg[6]  <= 32'hABCD1235;
+        memory_reg[7]  <= 32'h78945612;
+        memory_reg[8]  <= 32'h36952147;
+        memory_reg[9]  <= 32'hABCD9623;
+        memory_reg[10] <= 32'h32123423;
+        memory_reg[11] <= 32'h65423432;
+        memory_reg[12] <= 32'hB2343255;
+        memory_reg[13] <= 32'hA2321543;
+        memory_reg[14] <= 32'hBC965217;
+        memory_reg[15] <= 32'hAC598413;
+        memory_reg[16] <= 32'h56446ACD;
+        memory_reg[17] <= 32'h95123489;
+        memory_reg[18] <= 32'hFF2844FF;
+        memory_reg[19] <= 32'hABCDEFFF;
+        memory_reg[20] <= 32'h123FFACB;
+        memory_reg[21] <= 32'h78945612;
+        memory_reg[22] <= 32'h321ABCDE;
+        memory_reg[23] <= 32'h12FFFFFF;
+        memory_reg[24] <= 32'h12359FAB;
+        memory_reg[25] <= 32'h1432FDAC;
+        memory_reg[26] <= 32'h123ACDFF;
+        memory_reg[27] <= 32'hEEEFFFAA;
+        memory_reg[28] <= 32'hBBCCAAFF;
+        memory_reg[29] <= 32'h11223344;
+        memory_reg[30] <= 32'h55667788;
+        memory_reg[31] <= 32'h12345678;
+        memory_reg[32] <= 32'h12345678;
+        memory_reg[33] <= 32'h87654321;
+        memory_reg[34] <= 32'h12121212;
+        memory_reg[35] <= 32'h8A4FBCD1;
+        memory_reg[36] <= 32'h92374DAB;
+        memory_reg[37] <= 32'h15964278;
+        memory_reg[38] <= 32'hABCD1235;
+        memory_reg[39] <= 32'h78945612;
+        memory_reg[40] <= 32'h36952147;
+        memory_reg[41] <= 32'hABCD9623;
+        memory_reg[42] <= 32'h32123423;
+        memory_reg[43] <= 32'h65423432;
+        memory_reg[44] <= 32'hB2343255;
+        memory_reg[45] <= 32'hA2321543;
+        memory_reg[46] <= 32'hBC965217;
+        memory_reg[47] <= 32'hAC598413;
+        memory_reg[48] <= 32'h56446ACD;
+        memory_reg[49] <= 32'h95123489;
+        memory_reg[50] <= 32'hFF2844FF;
+        memory_reg[51] <= 32'hABCDEFFF;
+        memory_reg[52] <= 32'h123FFACB;
+        memory_reg[53] <= 32'h78945612;
+        memory_reg[54] <= 32'h321ABCDE;
+        memory_reg[55] <= 32'h12FFFFFF;
+        memory_reg[56] <= 32'h12359FAB;
+        memory_reg[57] <= 32'h1432FDAC;
+        memory_reg[58] <= 32'h123ACDFF;
+        memory_reg[59] <= 32'hEEEFFFAA;
+        memory_reg[60] <= 32'hBBCCAAFF;
+        memory_reg[61] <= 32'h11223344;
+        memory_reg[62] <= 32'h55667788;
+        memory_reg[63] <= 32'h99AABBCC;
+    end
+end
 
 endmodule 
